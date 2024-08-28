a6b5f8e7f5b449b157c96cdabee956569c979b44
Choi-Shin <shincho1990@naver.com>
2024-08-28 21:32:04 +0900

    aaaa

---
+++ src/main/java/com/api/albion/model/Item.java
@@ -0,0 +1,16 @@
+package com.api.albion.model;
+
+public class Item {
+    String city;
+    String uniqueName;
+    int price;
+
+    int quality;
+    int sellMin;
+    int sellMinDate;
+
+    int sellMax;
+    int sellMaxDate;
+    int buyMin;
+    int buyMinDate;
+}
---
+++ src/test/java/com/api/albion/AlbionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.albion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
